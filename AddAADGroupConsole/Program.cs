c4d258b88f0d53cfc31e78f250dad2d40f33d5db
Duygu Silivri (AKMAN) <duygua@microsoft.com>
2020-11-15 22:53:28 +0300

    Add project files.

---
+++ AddAADGroupConsole/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.Client;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.Framework.Client;
+using Microsoft.TeamFoundation.Framework.Common;
+
+namespace AddAADGroupConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string orgUrl = "";
+            Console.WriteLine("Organization Url");
+            orgUrl = Console.ReadLine();
+            string fileName = "";
+            Console.WriteLine("File Path:");
+            fileName = Console.ReadLine();
+
+            AddAADGroupToProjectsFromFile(orgUrl, fileName);
+
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+
+        public static void AddAADGroupToProjectsFromFile(string OrgUrl, string fileName)
+        {
+            //Create COM Objects. Create a COM object for everything that is referenced
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(fileName);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+
+            //iterate over the rows and columns and print to the console as it appears in the file
+            //excel is not zero based!!
+            //assuming that the first row is the title row
+            for (int i = 2; i <= rowCount; i++)
+            {
+                string teamProject = "";
+                string aadGroupName = "";
+                string customTpGroupName = "";
+                for (int j = 1; j <= colCount; j++)
+                {
+                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                    {
+                        if (j == 1)
+                            teamProject = xlRange.Cells[i, j].Value2.ToString();
+                        else if (j == 2)
+                            customTpGroupName = xlRange.Cells[i, j].Value2.ToString();
+                        else if (j == 3)
+                            aadGroupName = xlRange.Cells[i, j].Value2.ToString();
+                    }
+                }
+
+                AddAADGroupToTPCustomGroup(teamProject, customTpGroupName, aadGroupName, OrgUrl);
+            }
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //rule of thumb for releasing com objects:
+            //  never use two dots, all COM objects must be referenced and released individually
+            //  ex: [somthing].[something].[something] is bad
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+        }
+
+        public static bool AddAADGroupToTPCustomGroup(string teamProject, string tpCustomGroupName, string aadGroupName, string organizationUrl)
+        {
+            // Interactively ask the user for credentials, caching them so the user isn't constantly prompted
+            VssCredentials creds = new VssClientCredentials();
+            creds.Storage = new VssClientCredentialStorage();
+
+            var tpc = new TfsTeamProjectCollection(new Uri(organizationUrl), creds);
+            tpc.Connect(Microsoft.TeamFoundation.Framework.Common.ConnectOptions.IncludeServices);
+
+            // Get the TFS Identity Management Service
+            IIdentityManagementService ims = tpc.GetService<IIdentityManagementService>();
+
+            string tpCustomGroupNameFull = "[" + teamProject + "]" + "\\" + tpCustomGroupName;
+            string aadGroupNameFull = "[TEAM FOUNDATION]" + "\\" + aadGroupName;  //for AAD Groups
+
+            try
+            {
+                var tfsGroupIdentity = ims.ReadIdentity(IdentitySearchFactor.AccountName,
+                                                    tpCustomGroupNameFull,
+                                                    MembershipQuery.None,
+                                                    ReadIdentityOptions.IncludeReadFromSource);
+
+                var aadGroupIdentity = ims.ReadIdentity(IdentitySearchFactor.AccountName,
+                                                        aadGroupNameFull,
+                                                        MembershipQuery.None,
+                                                        ReadIdentityOptions.IncludeReadFromSource);
+
+                ims.AddMemberToApplicationGroup(tfsGroupIdentity.Descriptor, aadGroupIdentity.Descriptor);
+
+                Console.WriteLine("Group added: " + aadGroupName + " to " + tpCustomGroupName);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Group cannot be added: " + aadGroupName + ", " + tpCustomGroupName);
+                return false;
+            }
+        }
+    }
+}
